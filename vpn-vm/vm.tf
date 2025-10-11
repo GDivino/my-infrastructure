888e04200785f78e358b337848c48c6ce9729819
Gio Divino <giodivino.tech@gmail.com>
2025-10-12 00:19:36 +0800

    fix tags for s3-backend and vpn-vm

--- vpn-vm/vm.tf
+++ vpn-vm/vm.tf
@@ -1,5 +1,26 @@
+locals {
+  default_tags = {
+    ManagedBy = "terraform"
+    TFProject = join("//", [
+      "github.com/gdivino/my-infrastructure",
+      "vpn-vm/",
+    ])
+  }
+}
+
 resource "oci_core_instance" "wireguard" {
+  display_name        = "homelab-wireguard-server"
   availability_domain = var.availability_domain
   compartment_id      = var.compartment_id
   shape               = "VM.Standard.A1.Flex"
+
+  freeform_tags = merge(local.default_tags, { project = "homelab" })
+}
+
+resource "oci_core_vcn" "homelab" {
+  display_name   = "homelab-vcn"
+  compartment_id = var.compartment_id
+  cidr_blocks    = ["142.0.0.0/16"]
+
+  freeform_tags = merge(local.default_tags, { project = "homelab" })
 }
