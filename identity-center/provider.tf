75928c62a298483136d73e93186d916c6b04811d
Gio Divino <giodivino.tech@gmail.com>
2025-10-11 23:52:19 +0800

    update provider versions of AWS identity center

--- identity-center/provider.tf
+++ identity-center/provider.tf
@@ -1,20 +1,20 @@
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = ">= 1.13.3"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.20.0"
+      version = "~> 6.16.0"
     }
   }
 
   backend "s3" {
-    encrypt        = true
-    bucket         = "jing-infrastructure-state"
-    region         = "ap-southeast-1"
-    key            = "my-infrastructure/identity-center/terraform.tfstate"
-    dynamodb_table = "jing-infrastructure-state-lock"
-    profile        = "jing"
+    encrypt      = true
+    bucket       = "jing-infrastructure-state"
+    region       = "ap-southeast-1"
+    key          = "my-infrastructure/identity-center/terraform.tfstate"
+    use_lockfile = true
+    profile      = "jing"
   }
 }
 
