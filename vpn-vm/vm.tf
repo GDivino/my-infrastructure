7644978b272119dbcf0cba6250eec920287583c5
Gio Divino <giodivino.tech@gmail.com>
2025-10-12 11:57:02 +0800

    add ocpu shape config

--- vpn-vm/vm.tf
+++ vpn-vm/vm.tf
@@ -13,6 +13,9 @@ resource "oci_core_instance" "wireguard" {
   availability_domain = var.availability_domain
   compartment_id      = var.compartment_id
   shape               = "VM.Standard.A1.Flex"
+  shape_config {
+    ocpus = 1
+  }
 
   freeform_tags = merge(local.default_tags, { project = "homelab" })
 }
